b1d83bcb21bee6a08fa1ba38813c1a8f5854c1f4
cello-wek <falya500@gmail.com>
2021-08-20 11:35:08 +0300

    feat: change js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+	if(array === undefined){
+		return 0;
+	}
+	let mn = array.length > 0 ? array[0] : 0;
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] < mn) {
+			mn = array[i];
+		}
+	}
+	return mn;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+	if(array === undefined){
+		return 0;
+	}
+	let mx = array.length > 0 ? array[0] : 0;
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] > mx) {
+			mx = array[i];
+		}
+	}
+	return mx;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+	if(array === undefined){
+		return 0;
+	}
+	let sum = 0;
+	for (let i = 0; i < array.length; i++) {
+		sum += array[i];
+	}
+	let average = array.length ? sum / array.length : 0;
+	return average;
 }
